15df6e6ea6469cef04b0524c2dd18c9212336ef5
Muhammad Ahmed <64797138+mahmed74@users.noreply.github.com>
2021-11-05 21:37:03 +0500

    first

---
+++ server.mjs
@@ -0,0 +1,61 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+let users = [];
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.post("/user", (req, res) => {
+  if (!req.body.name || !req.body.email || !req.body.address) {
+    res.status(400).send("invalid data");
+  } else {
+    users.unshift({
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address,
+    });
+    res.send("user Created!");
+  }
+});
+
+app.put("/user/:id", (req, res) => {
+  if (users[req.params.id]) {
+    if (req.body.name) {
+      users[req.params.name] = req.body.name;
+    }
+    if (req.params.email) {
+      users[req.params.email] = req.body.email;
+    }
+    if (req.params.address) {
+      users[req.params.address];
+    }
+
+    res.send(users[req.params.id]);
+  } else {
+    res.send("user not found");
+  }
+});
+
+app.delete("/user/:id", (req, res) => {
+  if (users[req.params.id]) {
+    users[req.params.id] = {};
+    res.send("user deleted!");
+  } else {
+    res.send("user not found");
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`listening to http://localhost:${port}`);
+});
